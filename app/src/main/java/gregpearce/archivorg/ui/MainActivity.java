ba24cb88baf86306698b32eb1f764aa52300bf49
Greg Pearce <gregrpearce@gmail.com>
2016-07-20 11:15:30 +0100

    SearchView + Navigation drawer boilerplate.

--- app/src/main/java/gregpearce/archivorg/ui/MainActivity.java
+++ app/src/main/java/gregpearce/archivorg/ui/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.lapism.searchview.SearchView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +18,7 @@ public class MainActivity extends BaseActivity {
   @BindView(R.id.toolbar) Toolbar toolbar;
   @BindView(R.id.view_pager) ViewPager viewPager;
   @BindView(R.id.tab_layout) TabLayout tabLayout;
+  @BindView(R.id.search_view) SearchView searchView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -26,5 +31,21 @@ public class MainActivity extends BaseActivity {
     MainPagerAdapter sectionsPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
     viewPager.setAdapter(sectionsPagerAdapter);
     tabLayout.setupWithViewPager(viewPager);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_search: {
+        searchView.open(true);
+        return true;
+      }
+      default:
+        return super.onOptionsItemSelected(item);
+    }
   }
 }
