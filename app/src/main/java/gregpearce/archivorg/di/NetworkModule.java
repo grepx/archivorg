fec262e1c5292e0e7879c5534ec366b776eff735
Greg Pearce <gregrpearce@gmail.com>
2016-09-28 12:43:38 +0100

    Add http proxy capability

--- app/src/main/java/gregpearce/archivorg/di/NetworkModule.java
+++ app/src/main/java/gregpearce/archivorg/di/NetworkModule.java
@@ -2,10 +2,13 @@ package gregpearce.archivorg.di;
 
 import dagger.Module;
 import dagger.Provides;
+import gregpearce.archivorg.BuildConfig;
 import gregpearce.archivorg.domain.network.FeedServiceFactory;
 import gregpearce.archivorg.network.ArchiveOrgApiV1;
 import gregpearce.archivorg.network.ArchiveOrgApiV2;
 import gregpearce.archivorg.network.FeedServiceFactoryImpl;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import javax.inject.Singleton;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -15,33 +18,51 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module public class NetworkModule {
-  @Provides @Singleton ArchiveOrgApiV1 provideArchiveOrgApiV1() {
+  @Provides @Singleton Proxy provideProxy() {
+    // if a http proxy is defined, parse the settings and configure a Proxy
+    String ipAndPort = BuildConfig.HTTP_PROXY_IP_AND_PORT;
+    if (ipAndPort != null && !ipAndPort.isEmpty()) {
+      String ip = ipAndPort.split(":")[0];
+      String port = ipAndPort.split(":")[1];
+      return new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ip, Integer.parseInt(port)));
+    }
+    // otherwise, configure without a Proxy
+    return Proxy.NO_PROXY;
+  }
+
+  @Provides @Singleton ArchiveOrgApiV1 provideArchiveOrgApiV1(Proxy proxy) {
     // add a custom http client to automatically add the output=json query param
-    OkHttpClient httpClient = new OkHttpClient.Builder().addInterceptor(chain -> {
-      HttpUrl originalUrl = chain.request().url();
-      HttpUrl url = originalUrl.newBuilder().addQueryParameter("output", "json").build();
+    OkHttpClient httpClient = new OkHttpClient.Builder()
+        .addInterceptor(chain -> {
+          HttpUrl originalUrl = chain.request().url();
+          HttpUrl url = originalUrl.newBuilder().addQueryParameter("output", "json").build();
 
-      Request.Builder requestBuilder = chain.request().newBuilder().url(url);
-      Request request = requestBuilder.build();
-      return chain.proceed(request);
-    }).build();
+          Request.Builder requestBuilder = chain.request().newBuilder().url(url);
+          Request request = requestBuilder.build();
+          return chain.proceed(request);
+        })
+        .proxy(proxy)
+        .build();
 
-    Retrofit retrofit = new Retrofit.Builder().baseUrl("https://archive.org/")
-                                              .addConverterFactory(GsonConverterFactory.create())
-                                              .addCallAdapterFactory(
-                                                  RxJavaCallAdapterFactory.create())
-                                              .client(httpClient)
-                                              .build();
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("https://archive.org/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .client(httpClient)
+        .build();
 
     return retrofit.create(ArchiveOrgApiV1.class);
   }
 
-  @Provides @Singleton ArchiveOrgApiV2 provideArchiveOrgApiV2() {
-    Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.archivelab.org/")
-                                              .addConverterFactory(GsonConverterFactory.create())
-                                              .addCallAdapterFactory(
-                                                  RxJavaCallAdapterFactory.create())
-                                              .build();
+  @Provides @Singleton ArchiveOrgApiV2 provideArchiveOrgApiV2(Proxy proxy) {
+    OkHttpClient httpClient = new OkHttpClient.Builder().proxy(proxy).build();
+
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("https://api.archivelab.org/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .client(httpClient)
+        .build();
 
     return retrofit.create(ArchiveOrgApiV2.class);
   }
