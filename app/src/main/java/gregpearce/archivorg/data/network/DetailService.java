5b5a8cb8c8c112821afa4b8405ca6c95a2273226
Greg Pearce <gregrpearce@gmail.com>
2016-10-26 17:03:06 +0100

    Cache network results for detail service

--- app/src/main/java/gregpearce/archivorg/data/network/DetailService.java
+++ app/src/main/java/gregpearce/archivorg/data/network/DetailService.java
@@ -1,5 +1,6 @@
 package gregpearce.archivorg.data.network;
 
+import android.util.LruCache;
 import gregpearce.archivorg.domain.model.ArchiveFile;
 import gregpearce.archivorg.domain.model.ArchiveItem;
 import gregpearce.archivorg.util.NullUtil;
@@ -10,45 +11,58 @@ import javax.inject.Singleton;
 import rx.Observable;
 
 @Singleton public class DetailService {
+
   private ArchiveOrgApiV2 api;
+  // used to temporarily cache network results in memory for when they are requeried
+  private LruCache<String, Observable<ArchiveItem>> cache = new LruCache<>(5);
 
   @Inject DetailService(ArchiveOrgApiV2 api) {
     this.api = api;
   }
 
   public Observable<ArchiveItem> get(String id) {
-    return api.items(id)
-              // retry on network failure 3 times
-              .retry(3)
-              // map the network response to the domain model
-              .map(response -> {
-                List<ArchiveFile> files = new ArrayList<>(response.files.size());
-                for (ItemResponse.File file : response.files) {
-                  files.add(ArchiveFile.builder()
-                                       .name(file.name)
-                                       .size(file.size)
-                                       .length(file.length)
-                                       .source(file.source)
-                                       .format(file.format)
-                                       .build());
-                }
+    // if the result is already cached used that, otherwise query the network
+    Observable<ArchiveItem> result = cache.get(id);
+    if (result == null) {
+      result = api.items(id)
+                  // retry on network failure 3 times
+                  .retry(3)
+                  // map the network response to the domain model
+                  .map(response -> {
+                    List<ArchiveFile> files = new ArrayList<>(response.files.size());
+                    for (ItemResponse.File file : response.files) {
+                      files.add(ArchiveFile.builder()
+                                           .name(file.name)
+                                           .size(file.size)
+                                           .length(file.length)
+                                           .source(file.source)
+                                           .format(file.format)
+                                           .build());
+                    }
 
-                ItemResponse.Metadata metadata = response.metadata;
+                    ItemResponse.Metadata metadata = response.metadata;
 
-                // links in description HTML are mangled - manually fix them
-                String description = NullUtil.defaultValue(metadata.description).replace("//web.archive.org", "https://web.archive.org");
+                    // links in description HTML are mangled - manually fix them
+                    String description =
+                        NullUtil.defaultValue(metadata.description)
+                                .replace("//web.archive.org", "https://web.archive.org");
 
-                return ArchiveItem.builder()
-                                  .id(id)
-                                  .title(NullUtil.defaultValue(metadata.title))
-                                  .description(description)
-                                  .publishedDate(ArchiveOrgUtil.parseDateApiV2(metadata.publicdate))
-                                  .mediaType(ArchiveOrgUtil.parseMediaType(metadata.mediatype,
-                                                                           metadata.type))
-                                  .creator(NullUtil.defaultValue(metadata.creator))
-                                  .uploader(NullUtil.defaultValue(metadata.uploader))
-                                  .files(files)
-                                  .build();
-              });
+                    return ArchiveItem.builder()
+                                      .id(id)
+                                      .title(NullUtil.defaultValue(metadata.title))
+                                      .description(description)
+                                      .publishedDate(
+                                          ArchiveOrgUtil.parseDateApiV2(metadata.publicdate))
+                                      .mediaType(ArchiveOrgUtil.parseMediaType(metadata.mediatype,
+                                                                               metadata.type))
+                                      .creator(NullUtil.defaultValue(metadata.creator))
+                                      .uploader(NullUtil.defaultValue(metadata.uploader))
+                                      .files(files)
+                                      .build();
+                  })
+                  .cache();
+      cache.put(id, result);
+    }
+    return result;
   }
 }
---
+++ app/src/main/java/gregpearce/archivorg/util/ObservableCache.java
@@ -0,0 +1,8 @@
+package gregpearce.archivorg.util;
+
+/**
+ * Takes care of caching Observable so they can be reused instead of doing the work again.
+ */
+public class ObservableCache<T> {
+
+}
